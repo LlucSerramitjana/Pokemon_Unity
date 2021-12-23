6e8d4d08633bf115846b220d72009645fbce01e6
LlucSerramitjana <lluc.serramitjana@estudiantat.upc.edu>
2021-12-23 11:43:38 +0100

    NPC Dialog Added

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -13,10 +13,17 @@ public class BattleUnit : MonoBehaviour
     
     public void Setup()
     {
+        
         Pokemon = new Pokemon(_base, level);
         if (isPlayerUnit)
+        {
             GetComponent<Image>().sprite = Pokemon.Base.BackSprite;
+        }
         else
+        {
             GetComponent<Image>().sprite = Pokemon.Base.FrontSprite;
+        }
+
+        //image.color = originalColor;
     }
 }
